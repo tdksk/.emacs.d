1759051967bdc9d4f692777b4671161be3a29250
Keisuke Tada <tdkskdt@gmail.com>
2012-04-24 15:40:42 +0900

    Add MATLAB mode

--- config/lisp.el
+++ config/lisp.el
@@ -242,3 +242,9 @@
 ;; %' - truncated
 ;; %f - source
 ;; %# - id
+
+
+;;; MATLAB mode
+(autoload 'matlab-mode "matlab" "Enter Matlab mode." t)
+(setq auto-mode-alist (cons '(".m" . matlab-mode) auto-mode-alist))
+(autoload 'matlab-shell"matlab" "Interactive Matlab mode." t)
