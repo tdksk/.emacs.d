0f411799c358d23ad6cc61e22a1e0d7aac29e196
Keisuke Tada <tdkskdt@gmail.com>
2012-10-16 14:44:23 +0900

    Change warning face in js2-mode

--- config/lisp.el
+++ config/lisp.el
@@ -240,7 +240,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (defface js2-warning-face
-  `((t (:foreground "yellow")))
+  `((t (:foreground "magenta")))
   "Face for JavaScript warnings."
   :group 'js2-mode)
 (add-hook 'js2-mode-hook
