1ebcd167e5dceae4cc2488fe2af883c734567261
Keisuke Tada <tdkskdt@gmail.com>
2013-01-27 14:50:07 +0900

    Add switch-to-last-buffer

--- init.el
+++ init.el
@@ -39,6 +39,7 @@
 (define-key global-map (kbd "M-k") 'kill-line-ex)           ; 1行kill
 (define-key global-map (kbd "M-y") 'yank-bottom-line)       ; 下の行にyank
 (define-key global-map (kbd "C-x C-k") 'kill-buffer)        ; バッファ削除
+(define-key global-map (kbd "C-z") 'switch-to-last-buffer)  ; 直前のバッファに切り替え
 (define-key global-map (kbd "C-t") 'other-window-or-split)  ; ウィンドウ間移動(ウィンドウが1つのときは分割して移動)
 (define-key global-map (kbd "C-c t") 'swap-screen)          ; 分割したバッファを入れ替える
 (define-key global-map (kbd "C-c l") 'global-linum-mode)    ; linum-mode (global)
@@ -105,6 +106,10 @@
   (forward-line)
   (yank)
   (previous-line))
+
+(defun switch-to-last-buffer ()
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
 
 (defun other-window-or-split ()
   (interactive)
