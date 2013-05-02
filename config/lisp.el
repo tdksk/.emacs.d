c3e91d0dd47598378e4f34ebe09f5c73944fcf62
Keisuke Tada <tdkskdt@gmail.com>
2013-05-03 00:06:19 +0900

    Replace coffee-mode as submodule and add config of indentation in coffee-mode

--- config/lisp.el
+++ config/lisp.el
@@ -375,6 +375,12 @@
 ;;; CoffeeScript Mode
 (autoload 'coffee-mode "coffee-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (set (make-local-variable 'tab-width) 2)
+  (setq coffee-tab-width 2))
+(add-hook 'coffee-mode-hook
+          '(lambda() (coffee-custom)))
 
 ;;; MMM Mode
 ;; (require 'mmm-auto)
