dedb0ea795aeffefcf3dd4518d248529ed38a35b
Keisuke Tada <tdkskdt@gmail.com>
2013-06-03 16:14:59 +0900

    Modify popwin configurations for smart-compile and quickrun

--- config/lisp.el
+++ config/lisp.el
@@ -81,7 +81,8 @@
 ;; 対象
 (push 'help-mode popwin:special-display-config)
 (push '(completion-list-mode :noselect t) popwin:special-display-config)
-(push '(compilation-mode :noselect t) popwin:special-display-config)
+(push '(compilation-mode :position right :width .5 :noselect t) popwin:special-display-config)
+(push '("*quickrun*" :position right :width .5 :noselect t) popwin:special-display-config)
 (push '("*Warnings*") popwin:special-display-config)
 (push '("*Process List*") popwin:special-display-config)
 (push '("*helm*" :regexp t :height 20) popwin:special-display-config)
