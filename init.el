86069abb4c7f0b1a45cd29e52baf595b1dfdd134
Keisuke Tada <tdkskdt@gmail.com>
2013-06-11 20:43:10 +0900

    Add configurations of sharing clipboard on OS X

--- init.el
+++ init.el
@@ -989,5 +989,19 @@ Argument REPLACE String used to replace the matched strings in the buffer.
 (unless (server-running-p)
   (server-start))
 
+;;; OS X
+(setq darwin-p (eq system-type 'darwin))
+(when darwin-p
+  ;; clipboard
+  (defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+  (defun paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
+
 ;;; 非標準Elispの設定
 (load "config/lisp")
