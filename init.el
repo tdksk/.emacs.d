188f056cbe1442ddc40ab3d3b3245f78d66a7ea2
Keisuke Tada <tdkskdt@gmail.com>
2013-09-11 15:52:22 +0900

    Disable backup and auto save

--- init.el
+++ init.el
@@ -432,6 +432,10 @@
 
 ;;; バックアップファイルを作らない
 (setq backup-inhibited t)
+(setq make-backup-files nil)
+
+;;; オートセーブファイルを作らない
+(setq auto-save-default nil)
 
 ;;; 終了時にオートセーブファイルを消す
 (setq delete-auto-save-files t)
