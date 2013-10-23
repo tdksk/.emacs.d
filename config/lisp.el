80037ff804cd69d83e40ea43cf82b9cba59e89ea
Keisuke Tada <keisuke-tada@cookpad.com>
2013-10-23 15:02:14 +0900

    Fix indent in ruby mode

--- config/lisp.el
+++ config/lisp.el
@@ -656,6 +656,21 @@
 
 ;;; Ruby Mode
 (require 'ruby-end)
+(setq ruby-deep-indent-paren-style nil)
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
 (defun ruby-anti-hash-rocket ()
   (interactive)
   (save-excursion
