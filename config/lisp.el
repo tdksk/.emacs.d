c2017642204e75f89d5134e02993fe7f6b6b9aaa
Keisuke Tada <keisuke-tada@cookpad.com>
2013-12-04 09:47:31 +0900

    Update auto-mode-alist in ruby mode

--- config/lisp.el
+++ config/lisp.el
@@ -663,9 +663,9 @@
 (autoload 'ruby-mode "ruby-mode"
   "Mode for editing ruby source files" t)
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
 (setq ruby-insert-encoding-magic-comment nil)
 (require 'ruby-end)
 (require 'ruby-block)
