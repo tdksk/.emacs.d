ae9dbcaf19967bd6fea80c59b48272249fea77c0
Keisuke Tada <keisuke-tada@cookpad.com>
2014-10-10 18:54:24 +0900

    Enable ruby mode in Podfile

--- config/lisp.el
+++ config/lisp.el
@@ -683,6 +683,7 @@
 (add-to-list 'auto-mode-alist '("\\.jbuilder$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Podfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
 (setq ruby-insert-encoding-magic-comment nil)
 (require 'ruby-end)
