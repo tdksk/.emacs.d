ff15e5d37ada62f6b9650f6af35bc3b8a7d7cbfb
Keisuke Tada <keisuke-tada@cookpad.com>
2014-12-31 13:16:59 +0900

    Add migemo

--- config/lisp.el
+++ config/lisp.el
@@ -74,6 +74,17 @@
 ;;                 yatex-mode-hook))
 ;;   (add-hook hook 'ac-ispell-ac-setup))
 
+;;; migemo
+(require 'migemo)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+(migemo-init)
+
 ;;; smart-compile
 (require 'smart-compile)
 (global-set-key (kbd "C-c j") 'smart-compile)
