7fe22106cbbcb48eae275a79c102b6d43884a712
Keisuke Tada <keisuke-tada@haveagood.holiday>
2020-04-18 22:05:23 +0900

    Enable to load typescript-mode in ts and tsx

--- config/lisp.el
+++ config/lisp.el
@@ -702,7 +702,9 @@
              (local-set-key (kbd ";") 'coffee-open-line-below)))
 
 ;;; TypeScript Mode
-(require 'typescript-mode)
+(autoload 'typescript-mode "typescript-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.ts$" . typescript-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx$" . typescript-mode))
 (add-hook 'typescript-mode-hook
           (lambda ()
             (setq typescript-indent-level 2)))
